2dc9f0695d5baf3a4db0ea1076eb20c2664f5043
91820 <91820@LAPTOP-DTRV0N7K>
2021-09-22 18:52:59 +0530

    sporty shoes project structure designed

---
+++ src/main/java/com/simplilearn/sportyshoes/SportyShoesApplication.java
@@ -0,0 +1,40 @@
+package com.simplilearn.sportyshoes;
+
+
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@SpringBootApplication
+public class SportyShoesApplication {
+ 
+	@Bean
+	  public WebSecurityConfigurerAdapter webSecurityConfig(DataSource dataSource) {
+	      return new WebSecurityConfigurerAdapter() {
+	          @Override
+	          protected void configure(HttpSecurity http) throws Exception {
+	              http.authorizeRequests().antMatchers("/").permitAll().and()
+	                .authorizeRequests().antMatchers("/console/**").permitAll();
+	              http.csrf().disable();
+	              http.headers().frameOptions().disable();
+	   	          }
+
+	          @Override
+	          protected void configure(AuthenticationManagerBuilder builder) throws Exception {
+	              builder.jdbcAuthentication()
+	                     .dataSource(dataSource);
+	          }
+	      };
+	  }
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportyShoesApplication.class, args);
+	}
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+CREATE TABLE login (
+  id int NOT NULL AUTO_INCREMENT,
+  username varchar(15) DEFAULT NULL,
+  password varchar(15) DEFAULT NULL,
+  PRIMARY KEY (id)
+) ;
